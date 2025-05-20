64cdeacd5d71b0105543921ea934749896c7469a
Utkarsh Yadav <92722004+Alioth-Dev@users.noreply.github.com>
2025-05-20 11:32:23 +0530

    Add Random Choice Function

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello World")
+console.log("Hello World") // Testing
+
+function getComputerChoice() {
+    let value = Math.floor(Math.random()*10);
+    if (value < 4)
+        return "Rock"
+    else if (value > 3 && value < 7)
+        return "Paper"
+    else if (value > 6)
+        return "Scissors"   
+}
